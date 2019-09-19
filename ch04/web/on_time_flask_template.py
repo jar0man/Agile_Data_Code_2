d9ce41bf1973e7ef76ba8daf178164add62ac90f
Alonso Roman <jalonso.roman@gmail.com>
2019-09-19 12:08:53 +0100

    Fixed issues on chapter 04

--- ch04/web/on_time_flask_template.py
+++ ch04/web/on_time_flask_template.py
@@ -1,31 +1,37 @@
 import sys, os, re
+import time
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 from bson import json_util
 import config
 import json
 
-from pyelasticsearch import ElasticSearch
-elastic = ElasticSearch(config.ELASTIC_URL)
+from elasticsearch import Elasticsearch
+#elastic = Elasticsearch(config.ELASTIC_URL)
+elastic = Elasticsearch()
 
-# Process elasticsearch hits and return flights records
+# Process Elasticsearch hits and return flights records
 def process_search(results):
   records = []
+  total = 0
   if results['hits'] and results['hits']['hits']:
-    total = results['hits']['total']
+    total = results['hits']['total']['value']
     hits = results['hits']['hits']
     for hit in hits:
       record = hit['_source']
       records.append(record)
+
+  print('Total:'+ str(total) )     
   return records, total
 
 # Calculate offsets for fetching lists of flights from MongoDB
 def get_navigation_offsets(offset1, offset2, increment):
   offsets = {}
-  offsets['Next'] = {'top_offset': offset2 + increment, 'bottom_offset': 
-  offset1 + increment}
   offsets['Previous'] = {'top_offset': max(offset2 - increment, 0), 
  'bottom_offset': max(offset1 - increment, 0)} # Don't go < 0
+ 
+  offsets['Next'] = {'top_offset': offset2 + increment, 'bottom_offset': 
+  offset1 + increment}
   return offsets
 
 # Strip the existing start and end parameters from the query string
@@ -38,7 +44,7 @@ def strip_place(url):
 
 # Set up Flask and Mongo
 app = Flask(__name__)
-client = MongoClient()
+client = MongoClient() 
 
 # Controller: Fetch a flight and display it
 @app.route("/on_time_performance")
@@ -47,14 +53,14 @@ def on_time_performance():
   carrier = request.args.get('Carrier')
   flight_date = request.args.get('FlightDate')
   flight_num = request.args.get('FlightNum')
+ 
   
   flight = client.agile_data_science.on_time_performance.find_one({
     'Carrier': carrier,
     'FlightDate': flight_date,
-    'FlightNum': flight_num
+    'FlightNum': int(flight_num)
   })
-  
-  return render_template('flight.html', flight=flight)
+  return render_template('flight.html', flight=flight, carrier=carrier,flight_date=flight_date, flight_num=flight_num)
 
 # Controller: Fetch all flights between cities on a given day and display them
 @app.route("/flights/<origin>/<dest>/<flight_date>")
@@ -114,19 +120,19 @@ def search_flights():
   nav_path = strip_place(request.url)
   nav_offsets = get_navigation_offsets(start, end, config.RECORDS_PER_PAGE)
   
-  # Build the base of our elasticsearch query
+  # Build the base of our Elasticsearch query
   query = {
     'query': {
       'bool': {
         'must': []}
     },
-    'sort': [
-      {'FlightDate': {'order': 'asc', 'ignore_unmapped' : True} },
-      {'DepTime': {'order': 'asc', 'ignore_unmapped' : True} },
-      {'Carrier': {'order': 'asc', 'ignore_unmapped' : True} },
-      {'FlightNum': {'order': 'asc', 'ignore_unmapped' : True} },
-      '_score'
-    ],
+   # 'sort': [
+   #   {'FlightDate': {'order': 'asc', 'ignore_unmapped' : True} },
+   #   {'DepTime': {'order': 'asc', 'ignore_unmapped' : True} },
+   #   {'Carrier': {'order': 'asc', 'ignore_unmapped' : True} },
+   #   {'FlightNum': {'order': 'asc', 'ignore_unmapped' : True} },
+   #   '_score'
+   # ],
     'from': start,
     'size': config.RECORDS_PER_PAGE
   }
@@ -143,13 +149,13 @@ def search_flights():
   if tail_number: 
     query['query']['bool']['must'].append({'match': {'TailNum': tail_number}})
   if flight_number: 
-    query['query']['bool']['must'].append({'match': {'FlightNum': flight_number}})
+    query['query']['bool']['must'].append({'match': {'FlightNum': int(flight_number)}})
   
-  # Query elasticsearch, process to get records and count
+  # Query Elasticsearch, process to get records and count
   print("QUERY")
   print(carrier, flight_date, origin, dest, tail_number, flight_number)
   print(json.dumps(query))
-  results = elastic.search(query)
+  results = elastic.search(index='agile_data_science',body=query)
   flights, flight_count = process_search(results)
   
   # Persist search parameters in the form template
@@ -165,6 +171,7 @@ def search_flights():
     dest=dest,
     tail_number=tail_number,
     flight_number=flight_number
+    ,query=request.url
     )
 
 if __name__ == "__main__":
